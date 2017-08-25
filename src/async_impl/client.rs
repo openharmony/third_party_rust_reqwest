seanmonstar/reqwest

Support PKCS12 identity on the ClientBuilder

src/async_impl/client.rs
@@ -17,7 +17,7 @@ use super::response::{self, Response};
 use connect::Connector;
 use into_url::to_uri;
 use redirect::{self, RedirectPolicy, check_redirect, remove_sensitive_headers};
-use {Certificate, IntoUrl, Method, proxy, Proxy, StatusCode, Url};
+use {Certificate, Pkcs12, IntoUrl, Method, proxy, Proxy, StatusCode, Url};
 
 static DEFAULT_USER_AGENT: &'static str =
     concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
@@ -112,6 +112,20 @@ impl ClientBuilder {
     pub fn add_root_certificate(&mut self, cert: Certificate) -> ::Result<&mut ClientBuilder> {
         let cert = ::tls::cert(cert);
         try_!(self.config_mut().tls.add_root_certificate(cert));
+        Ok(self)
+    }
+
+    /// Sets the identity to be used for client certificate authentication.
+    ///
+    /// This can be used in mutual authentication scenarios to identify to a server
+    /// with a Pkcs12 archive containing a certificate and private key for example.
+    ///
+    /// # Errors
+    ///
+    /// This method fails if adding client identity was unsuccessful.
+    pub fn identity(&mut self, pkcs12: Pkcs12) -> ::Result<&mut ClientBuilder> {
+        let pkcs12 = ::tls::pkcs12(pkcs12);
+        try_!(self.config_mut().tls.identity(pkcs12));
         Ok(self)
     }
 

src/lib.rs
@@ -151,7 +151,7 @@ pub use self::proxy::Proxy;
 pub use self::redirect::{RedirectAction, RedirectAttempt, RedirectPolicy};
 pub use self::request::{Request, RequestBuilder};
 pub use self::response::Response;
-pub use self::tls::Certificate;
+pub use self::tls::{Certificate, Pkcs12};
 
 
 // this module must be first because of the `try_` macro

openharmony/third_party_rust_reqwest

src/async_impl/client.rs
@@ -17,7 +17,7 @@ use super::response::{self, Response};
 use connect::Connector;
 use into_url::to_uri;
 use redirect::{self, RedirectPolicy, check_redirect, remove_sensitive_headers};
-use {Certificate, IntoUrl, Method, proxy, Proxy, StatusCode, Url};
+use {Certificate, Identity, IntoUrl, Method, proxy, Proxy, StatusCode, Url};
 
 static DEFAULT_USER_AGENT: &'static str =
     concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
@@ -112,6 +112,20 @@ impl ClientBuilder {
     pub fn add_root_certificate(&mut self, cert: Certificate) -> ::Result<&mut ClientBuilder> {
         let cert = ::tls::cert(cert);
         try_!(self.config_mut().tls.add_root_certificate(cert));
+        Ok(self)
+    }
+
+    /// Sets the identity to be used for client certificate authentication.
+    ///
+    /// This can be used in mutual authentication scenarios to identify to a server
+    /// with a Pkcs12 archive containing a certificate and private key for example.
+    ///
+    /// # Errors
+    ///
+    /// This method fails if adding client identity was unsuccessful.
+    pub fn identity(&mut self, identity: Identity) -> ::Result<&mut ClientBuilder> {
+        let pkcs12 = ::tls::pkcs12(identity);
+        try_!(self.config_mut().tls.identity(pkcs12));
         Ok(self)
     }
 

src/lib.rs
@@ -153,7 +153,7 @@ pub use self::proxy::Proxy;
 pub use self::redirect::{RedirectAction, RedirectAttempt, RedirectPolicy};
 pub use self::request::{Request, RequestBuilder};
 pub use self::response::Response;
-pub use self::tls::Certificate;
+pub use self::tls::{Certificate, Identity};
 
 
 // this module must be first because of the `try_` macro